c66ff301e9077df53eba7261f478f3ae07a7e165
Karen Wei <karenweiy@gmail.com>
2019-09-23 21:05:09 -0400

    all charts ready

--- Visualization_NEW/Visualization_NEW/static/js/app.js
+++ Visualization_NEW/Visualization_NEW/static/js/app.js
@@ -16,7 +16,7 @@ var putdataroute = "/puts";
 function buildAllCallPlot() {
   var calldataroute = "/calls";
 
-  d3.json(calldataroute).then(function(response) {
+  d3.json(calldataroute).then(function (response) {
     // Create empty arrays to extract relevant data from reponse
     var strike = []
     var volatility = [];
@@ -26,14 +26,14 @@ function buildAllCallPlot() {
     console.log(response);
 
     // Convert numerical data points (from string to integer/float)
-    response.forEach(function(record, key){
+    response.forEach(function (record, key) {
       // console.log('key:', key);
       // console.log('record:', record);
 
       strike.push(+record['Strike']);
       volatility.push(+record['Implied_Volatility']);
       ticker.push(record['Ticker']);
-      size.push(+record['Implied_Volatility'] * 10);
+      size.push(+record['Implied_Volatility']*10);
     })
 
     console.log('volatility:', volatility);
@@ -43,10 +43,10 @@ function buildAllCallPlot() {
       y: volatility,
       // type: "scatter",
       mode: "markers",
-      hovertext:ticker,
-      marker:{
-        size:size,
-        color:strike
+      hovertext: ticker,
+      marker: {
+        size: size,
+        color: strike
       }
     };
 
@@ -54,63 +54,63 @@ function buildAllCallPlot() {
 
     var layout = {
       title: "Strike Price vs. Implied Volatility",
-      showlegend:false
+      showlegend: false
     }
 
     Plotly.newPlot("callbubble", callbubble, layout);
-    });
+  });
 
-  };
+};
 
-  function buildAllPutPlot() {
-    var putdataroute = "/puts";
-  
-    d3.json(putdataroute).then(function(response) {
-      // Create empty arrays to extract relevant data from reponse
-      var strike = []
-      var volatility = [];
-      var ticker = [];
-      var size = [];
-  
-      console.log(response);
-  
-      // Convert numerical data points (from string to integer/float)
-      response.forEach(function(record, key){
-        // console.log('key:', key);
-        // console.log('record:', record);
-  
-        strike.push(+record['Strike']);
-        volatility.push(+record['Implied_Volatility']);
-        ticker.push(record['Ticker']);
-        size.push(+record['Implied_Volatility'] * 10);
-      })
-  
-      console.log('volatility:', volatility);
-  
-      var trace = {
-        x: strike,
-        y: volatility,
-        // type: "scatter",
-        mode: "markers",
-        hovertext:ticker,
-        marker:{
-          size:size,
-          color:strike
-        }
-      };
-  
-      var putbubble = [trace];
-  
-      var layout = {
-        title: "Strike Price vs. Implied Volatility",
-        showlegend:false
+function buildAllPutPlot() {
+  var putdataroute = "/puts";
+
+  d3.json(putdataroute).then(function (response) {
+    // Create empty arrays to extract relevant data from reponse
+    var strike = []
+    var volatility = [];
+    var ticker = [];
+    var size = [];
+
+    console.log(response);
+
+    // Convert numerical data points (from string to integer/float)
+    response.forEach(function (record, key) {
+      // console.log('key:', key);
+      // console.log('record:', record);
+
+      strike.push(+record['Strike']);
+      volatility.push(+record['Implied_Volatility']);
+      ticker.push(record['Ticker']);
+      size.push(+record['Implied_Volatility']*10);
+    })
+
+    console.log('volatility:', volatility);
+
+    var trace = {
+      x: strike,
+      y: volatility,
+      // type: "scatter",
+      mode: "markers",
+      hovertext: ticker,
+      marker: {
+        size: size,
+        color: strike
       }
-  
-      Plotly.newPlot("putbubble", putbubble, layout);
-      });
-  
     };
-  
+
+    var putbubble = [trace];
+
+    var layout = {
+      title: "Strike Price vs. Implied Volatility",
+      showlegend: false
+    }
+
+    Plotly.newPlot("putbubble", putbubble, layout);
+  });
+
+};
+
 
 function init() {
   console.log('init() called!');
@@ -141,20 +141,89 @@ function optionChanged(newticker) {
   buildSinglePutPlot(newticker); // single-ticker plot function
 }
 
-// function buildSingleCallPlot(newticker) {
-// 	// Step 1: Create route string with newticker
-// 	let endpoint = "/calls/" + newticker
-	
-// 	// Step 2: Perform API call to the (Flask) route given the newticker
-// 	d3.json(endpoint).then(function(response){
-// 	  // Step 3: Process data and build plot!
-	  
-// 	}
-	
-//  }
+function buildSingleCallPlot(newticker) {
+  // Step 1: Create route string with newticker
+  let endpoint = "/calls/" + newticker
 
+  // Step 2: Perform API call to the (Flask) route given the newticker
+  d3.json(endpoint).then(function (response) {
+    // Step 3: Process data and build plot!
+    var openinterest = [];
+    var strike=[];
+    console.log(response);
+
+    response.forEach(function (record, key) {
+      // console.log('key:', key);
+      // console.log('record:', record);
+
+      openinterest.push(+record['Open_Interest']);
+      strike.push(+record['Strike'])
+
+    });
+    console.log('Open_Interest:', openinterest);
+
+    var trace = {
+      x: strike,
+      y: openinterest,
+      name: "Open Interest",
+      type: "bar",
+      // orientation: "h"
+    };
+
+    var callbar = [trace];
+
+    var layout = {
+      title: "Calls Open interest by Ticker",
+      showlegend: false
+    };
+
+    Plotly.newPlot("callbar", callbar, layout);
+
+  });
+}
+	
+function buildSinglePutPlot(newticker) {
+  // Step 1: Create route string with newticker
+  let endpoint = "/puts/" + newticker
+
+  // Step 2: Perform API call to the (Flask) route given the newticker
+  d3.json(endpoint).then(function (response) {
+    // Step 3: Process data and build plot!
+    var openinterest = [];
+    var strike=[];
+    console.log(response);
+
+    response.forEach(function (record, key) {
+      // console.log('key:', key);
+      // console.log('record:', record);
+
+      openinterest.push(+record['Open_Interest']);
+      strike.push(+record['Strike'])
+
+    });
+    console.log('Open_Interest:', openinterest);
+
+    var trace = {
+      x: strike,
+      y: openinterest,
+      name: "Open Interest",
+      type: "bar",
+      // orientation: "h"
+    };
+
+    var putbar = [trace];
+
+    var layout = {
+      title: "Puts Open interest by Ticker",
+      showlegend: false
+    };
+
+    Plotly.newPlot("putbar", putbar, layout);
+
+  });
+}
 
 // Execute Init()!
-init()
+init();
 buildAllCallPlot();
 buildAllPutPlot();
